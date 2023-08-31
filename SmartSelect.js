8991a6916e896a647cba6d8443faf4bead8f34fe
zaixu <zaixu91@gmail.com>
2023-08-31 17:54:02 +0900

    SmartSelect 개발중

--- SmartSelect.js
+++ SmartSelect.js
@@ -7,66 +7,49 @@
 
 class SmartSelect{
 	constructor(options){
+		this.controls = options.replace('#', '').replace('.', '');
 		this.select = document.querySelector(options);
 		this.btn = this.select.querySelector('button');
-		this.list = this.select.querySelector('ul');
+		this.lists = this.select.querySelector('ul');
+		this.changeCallback = null;
+
 		this.init();
 	}
 
-	on = {
-		'chageAfter': function(){
-			console.log('ok');
-		}
-	}
-
-	selected(e){
-		const value = {
-			text: null,
-			options: null,
-			index: null
+	updateSelected(target){
+		const result = {
+			currentValue: target.dataset.value,
+			index: target.dataset.index,
+			text: target.textContent
 		};
-		value.text = e.innerHTML;
-		value.options = e.dataset.option;
-		value.index = e.dataset.index;
 
-		this.btn.innerHTML = value.text;
-
-		this.toggle();
-		this.on.chageAfter();
+		this.btn.textContent = result.text;
+		this.expandToggle();
+		this.changeCallback?.(result);
 	}
 
-	toggle(){
-		if(!this.select.classList.contains('expanded')){
-			this.select.classList.add('expanded');
-			this.btn.ariaExpanded = true;
-		}else{
-			this.select.classList.remove('expanded');
-			this.btn.ariaExpanded = false;
-		}
+	afterChange(callback){
+		if(typeof callback === 'function') this.changeCallback = callback;
+	}
+
+	expandToggle(){
+		this.select.classList.toggle('expanded');
+  		this.btn.ariaExpanded = !this.btn.ariaExpanded;
 	}
 
 	init(){
-		const controls = this.select.className + '-list';
+		const controlConnect = this.controls + '-list';
 
-		// Button
 		this.btn.ariaExpanded = false;
 		this.btn.setAttribute('aria-haspopup', 'listbox');
-		this.btn.setAttribute('aria-controls', controls);
-		this.btn.addEventListener('click', () => {
-			this.toggle();
-		});
+		this.btn.setAttribute('aria-controls', controlConnect);
+		this.btn.addEventListener('click', () => this.expandToggle());
 
-		// List
-		this.list.id = controls;
-		this.list.role = 'listbox';
-
-		const lists = this.list.querySelectorAll('li');
-		lists.forEach((li, index) => {
-			li.dataset.index = index;
-
-			li.addEventListener('click', (e) => {
-				this.selected(e.target);
-			});
+		this.lists.id = controlConnect;
+		this.lists.role = 'listbox';
+		this.lists.querySelectorAll('li').forEach((list, index) => {
+			list.dataset.index = index;
+			list.addEventListener('click', e => this.updateSelected(e.target));
 		});
 	}
 }
