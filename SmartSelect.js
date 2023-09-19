60fed4b0f0ef83bbaea89e6896192356f2a8a2c5
zaixu <zaixu91@gmail.com>
2023-09-19 18:14:17 +0900

    SmartSelect 개발중

--- SmartSelect.js
+++ SmartSelect.js
@@ -12,44 +12,89 @@ class SmartSelect{
 		this.btn = this.select.querySelector('button');
 		this.lists = this.select.querySelector('ul');
 		this.changeCallback = null;
+		this.currentValue = null;
 
 		this.init();
 	}
 
-	updateSelected(target){
+	updateSelected(value){
+		const { prev, current } = value;
 		const result = {
-			currentValue: target.dataset.value,
-			index: target.dataset.index,
-			text: target.textContent
+			prev,
+			current: current.dataset.value,
+			index: current.dataset.smartIndex,
+			text: current.textContent
 		};
 
 		this.btn.textContent = result.text;
 		this.expandToggle();
 		this.changeCallback?.(result);
+		this.currentValue = result.current;
+	}
+
+	expandToggle(){
+		this.select.classList.toggle('expanded');
+
+		if(this.btn.ariaExpanded == 'false'){
+			this.btn.ariaExpanded = true;
+			// this.lists.querySelector('li:first-child').focus();
+		}else{
+			this.btn.ariaExpanded = false;
+		}
 	}
 
 	afterChange(callback){
 		if(typeof callback === 'function') this.changeCallback = callback;
 	}
 
-	expandToggle(){
-		this.select.classList.toggle('expanded');
-  		this.btn.ariaExpanded = !this.btn.ariaExpanded;
-	}
-
 	init(){
-		const controlConnect = this.controls + '-list';
+		const controlConnect = this.controls + '-smart-select';
 
 		this.btn.ariaExpanded = false;
-		this.btn.setAttribute('aria-haspopup', 'listbox');
+		this.btn.ariaHasPopup = 'listbox';
 		this.btn.setAttribute('aria-controls', controlConnect);
 		this.btn.addEventListener('click', () => this.expandToggle());
 
 		this.lists.id = controlConnect;
 		this.lists.role = 'listbox';
 		this.lists.querySelectorAll('li').forEach((list, index) => {
-			list.dataset.index = index;
-			list.addEventListener('click', e => this.updateSelected(e.target));
+			list.tabIndex = 0;
+			list.dataset.smartIndex = index;
+
+			list.addEventListener('click', e => {
+				const value = {
+					prev: this.currentValue,
+					current: e.target
+				};
+
+				this.updateSelected(value);
+			});
+
+			/*
+			list.addEventListener('keydown', e => {
+				const key = e.key.toLowerCase();
+
+				if(key == 'enter'){
+					const value = {
+						prev: this.currentValue,
+						current: e.target
+					};
+	
+					this.updateSelected(value);
+				}else if(key == 'arrowdown'){
+					list.nextElementSibling.focus();
+				}else if(key == 'arrowup'){
+					list.previousElementSibling.focus();
+				}
+			});
+			*/
+		});
+
+		// 다른 영역 클릭 시 select 닫음
+		this.select.addEventListener('click', e => e.stopPropagation());
+		document.addEventListener('click', () => {
+			const state = this.select.classList.contains('expanded');
+			if(state) this.expandToggle();
 		});
 	}
 }
